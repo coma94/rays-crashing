58190b3063c132815780a2994fc00098b11555be
Etienne Moutot <etienne.moutot@ens-lyon.org>
2016-12-02 17:35:50 +0100

    Classes Ray, Sphere and Camera

--- include/objects.h
+++ include/objects.h
@@ -11,6 +11,7 @@ public:
     Vector3(double x=0, double y=0, double z=0);
 
     Vector3 operator-() const;
+    Vector3 normalized() const;
 
     friend std::ostream& operator<<(std::ostream& out, Vector3 const& a);
 };
@@ -25,6 +26,46 @@ double sprod(Vector3 const& a, Vector3 const& b);
 Vector3 vprod(Vector3 const& a, Vector3 const& b);
 double norm(Vector3 v); 
 double normq(Vector3 v);
+
+
+
+class Ray
+{
+public:
+    Vector3 origin;
+    Vector3 direction;
+
+    Ray(Vector3 origin=Vector3(0,0,0), Vector3 direction=Vector3(0,0,0));
+};
+
+class Sphere
+{
+public:
+    Vector3 origin;
+    double radius;
+
+    Sphere(Vector3 origin=Vector3(0,0,0), double radius=1);
+};
+
+
+class Camera
+{
+public:
+    Vector3 origin;
+    double fov; // in radians
+    unsigned int image_width, image_height;
+
+    Camera(Vector3 origin=Vector3(0,0,0),
+	   double fov=1.05,
+	   unsigned int width=800,
+	   unsigned int height=600);
+
+protected:
+    /*
+      Generate a ray of norm 1 going to pixel (i,j)
+    */
+    Ray ray_to(unsigned int i, unsigned int j);
+};
 
 
 #endif
--- src/objects.cpp
+++ src/objects.cpp
@@ -2,15 +2,21 @@
 
 #include <cmath>
 
+// ##### VECTOR3 #####
 
 Vector3::Vector3(double x, double y, double z)
-: x(x), y(y), z(z)
+    : x(x), y(y), z(z)
 {
 }
 
 Vector3 Vector3::operator-() const
 {
     return Vector3(-x, -y, -z);
+}
+
+Vector3 Vector3::normalized() const
+{
+    return (*this)/norm(*this);
 }
 
 Vector3 operator+(Vector3 const& a, Vector3 const& b)
@@ -62,9 +68,41 @@ Vector3 vprod(Vector3 const& a, Vector3 const& b)
 
 double norm(Vector3 v)
 {
-  return sqrt(normq(v));
+    return sqrt(normq(v));
 }
 double normq(Vector3 v)
 {
-  return v.x*v.x + v.y*v.y + v.z*v.z;
+    return v.x*v.x + v.y*v.y + v.z*v.z;
+}
+
+
+
+// ##### RAY #####
+Ray::Ray(Vector3 origin, Vector3 direction)
+    : origin(origin), direction(direction)
+{
+}
+
+// ##### SPHERE #####
+Sphere::Sphere(Vector3 origin, double radius)
+    : origin(origin), radius(radius)
+{
+}
+
+
+// ##### CAMERA #####
+Camera::Camera(Vector3 origin,
+       double fov,
+       unsigned int width,
+       unsigned int height)
+    : origin(origin), fov(fov), image_width(width), image_height(height)
+{
+}
+
+
+Ray Camera::ray_to(unsigned int i, unsigned int j)
+{
+    return Ray(origin, Vector3(j-image_width/2+0.5,
+			       i-image_height/2+0.5,
+			       -image_height/(2*tan(fov/2.))));
 }
